64f483cd4314a1b45cccc7983f0baffd128ce38e
Mukesh Sahani <ms6148832@gmail.com>
2026-07-04 13:28:38 +0530

    Update TaskmanagerApplication.java

--- src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -3,11 +3,15 @@ package com.example.taskmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
 @EnableScheduling
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.example.taskmanager.controller", "com.example.taskmanager.Config"})
+@ComponentScan(basePackages = {
+    "com.example.taskmanager", 
+    "com.example.taskmanager.controller", 
+    "com.example.taskmanager.Config"
+})
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
